1c195dcb39de827212710debd5d9eb90425bf8af
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-03 16:33:05 +0600

    dynamic or any types

--- output/script.js
+++ output/script.js
@@ -51,21 +51,46 @@
     // console.log(a);
     // ** Union types
     // Normal Variable
-    let b;
-    //array
-    let a = [];
-    b = "Sifat";
-    console.log(b);
+    // let b: string | number;
+    // //array
+    // let a: (string | number)[] = [];
+    // b = "Sifat";
+    // console.log(b);
     // a = ["Sahabul Islam Sifat", 22];
     // console.log(a);
     // a.push('Sifat', 22, false)
     // Object
-    let c;
-    // c=[]
-    c = {
-        name: "SIfat",
-        age: 22,
-        isAdmin: true,
-    };
-    console.log(c);
+    // let c: {
+    //   name: string;
+    //   age: number;
+    //   isAdmin: boolean;
+    // };
+    // // c=[]
+    // c = {
+    //   name: "SIfat",
+    //   age: 22,
+    //   isAdmin: true,
+    // };
+    // console.log(c);
+    //** Dynamic Type or Any Type */
+    // let a;
+    // a = 2;
+    // a = "Sifat";
+    // let a: any;
+    // a = "Sifat";
+    // console.log(a);
+    // ARRAY
+    // let a: any[] = [];
+    // a.push("SIfat");
+    // a.push(22);
+    // a.push(true);
+    // console.log(a);
+    // OBJECT
+    // let b: any = {};
+    // b = {
+    //   name: "Sifat",
+    //   age: 22,
+    //   isSingle: true,
+    // };
+    // console.log(b);
 }
--- src/script.ts
+++ src/script.ts
@@ -8,7 +8,6 @@
   //   );
   // };
   // technologies("Javascript", "September, 1995");
-
   // console.log("Hello");
   // const country = "Bangladesh";
   // console.log(country);
@@ -42,45 +41,55 @@
   //   isCapital: true,
   // };
   // person.country("Rangpur");
-
   // let a;
   // a = 5;
   // a = "string";
-
   // let a: string;
   // let b: number;
-
   // a = "Ayan";
   // console.log(a);
-
   // ** Union types
-
   // Normal Variable
-  let b: string | number;
-
-  //array
-  let a: (string | number)[] = [];
-
-  b = "Sifat";
-  console.log(b);
-
+  // let b: string | number;
+  // //array
+  // let a: (string | number)[] = [];
+  // b = "Sifat";
+  // console.log(b);
   // a = ["Sahabul Islam Sifat", 22];
   // console.log(a);
-
   // a.push('Sifat', 22, false)
-
   // Object
-  let c: {
-    name: string;
-    age: number;
-    isAdmin: boolean;
-  };
-
-  // c=[]
-  c = {
-    name: "SIfat",
-    age: 22,
-    isAdmin: true,
-  };
-  console.log(c);
+  // let c: {
+  //   name: string;
+  //   age: number;
+  //   isAdmin: boolean;
+  // };
+  // // c=[]
+  // c = {
+  //   name: "SIfat",
+  //   age: 22,
+  //   isAdmin: true,
+  // };
+  // console.log(c);
+  //** Dynamic Type or Any Type */
+  // let a;
+  // a = 2;
+  // a = "Sifat";
+  // let a: any;
+  // a = "Sifat";
+  // console.log(a);
+  // ARRAY
+  // let a: any[] = [];
+  // a.push("SIfat");
+  // a.push(22);
+  // a.push(true);
+  // console.log(a);
+  // OBJECT
+  // let b: any = {};
+  // b = {
+  //   name: "Sifat",
+  //   age: 22,
+  //   isSingle: true,
+  // };
+  // console.log(b);
 }
