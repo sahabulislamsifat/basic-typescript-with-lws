bcd3bff35075f6908efa04a650c30f5fab8ebf64
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-03 14:38:26 +0600

    Type basic

--- output/script.js
+++ output/script.js
@@ -1,6 +1,6 @@
 "use strict";
 {
-    console.log("Hello");
+    // console.log("Hello");
     // const country = "Bangladesh";
     // console.log(country);
     // const technologies = (name: string, established: string) => {
@@ -9,4 +9,28 @@
     //   );
     // };
     // technologies("Javascript", "September, 1995");
+    // let playerName = "Mashrafi";
+    // console.log(playerName);
+    // playerName = 22;
+    // console.log(playerName);
+    // function multiply(a: number, b: number) {
+    //   return a * b;
+    // }
+    // console.log(multiply("result", 55));
+    // console.log(multiply(44, 55));
+    // ARRAY
+    // let fruits = ["apple", "Banana", "fruits", "Mango"];
+    // fruits.push(34);
+    // let mixed = [22, "Hello", true, {}];
+    // mixed.push(44, "world", false, {
+    //   name: "Sifat",
+    // });
+    // console.log(mixed);
+    // Object
+    let person = {
+        name: "Sifat",
+        age: 22,
+        isCapital: true,
+    };
+    person.country("Rangpur");
 }
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,6 @@
 {
-  console.log("Hello");
+  // console.log("Hello");
   // const country = "Bangladesh";
-
   // console.log(country);
   // const technologies = (name: string, established: string) => {
   //   console.log(
@@ -9,4 +8,30 @@
   //   );
   // };
   // technologies("Javascript", "September, 1995");
+  // let playerName = "Mashrafi";
+  // console.log(playerName);
+  // playerName = 22;
+  // console.log(playerName);
+  // function multiply(a: number, b: number) {
+  //   return a * b;
+  // }
+  // console.log(multiply("result", 55));
+  // console.log(multiply(44, 55));
+  // ARRAY
+  // let fruits = ["apple", "Banana", "fruits", "Mango"];
+  // fruits.push(34);
+  // let mixed = [22, "Hello", true, {}];
+  // mixed.push(44, "world", false, {
+  //   name: "Sifat",
+  // });
+  // console.log(mixed);
+
+  // Object
+  let person = {
+    name: "Sifat",
+    age: 22,
+    isCapital: true,
+  };
+
+  person.country("Rangpur");
 }
